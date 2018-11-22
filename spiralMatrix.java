3af8c7fbd13b4d1ad80a5d5dfee47da773c7f0c1
summers wu <wlysummers@gmail.com>
2018-11-22 00:07:36 -0500

    working version

--- spiralMatrix.java
+++ spiralMatrix.java
@@ -5,32 +5,47 @@ class Solution {
         int i = 0;//number of rows 
         int j = 0;//number of columns
         int rowCounter = 0; //which layer is this?
+        if (matrix.length == 0)
+        {return ans;}
+        if(matrix[0].length==0){return ans;}
+        
         for(int k=0; k < size; k++){
-            i = i+rowCounter;
-            j = j+rowCounter;
+
             ans.add(matrix[i][j]);
             //looping through the matrix and setting thing item as 
-            if(i==0+rowCounter && j != matrix[0].length - rowCounter){
+            if(i==0+rowCounter && j != matrix[0].length - rowCounter-1){
                 j++;
+                continue;
             }
-            if(j==(matrix[0].length - rowCounter) && i != (matrix.length - rowCounter)){
+            if(j==(matrix[0].length - rowCounter-1) && i != (matrix.length - rowCounter-1)){
                 i++;
+                
+                continue;
             }
             
-            if(i==(matrix.length-rowCounter) && j != (0 - rowCounter)){
+            if(i==(matrix.length-rowCounter-1) && j != (0 + rowCounter)){
                 j--;
+                
+                continue;
             }
             
-            if(j==(0 +rowCounter) && (i !=0 + rowCounter)){
+            if(j==(0 + rowCounter) && ((i-1) !=0 + rowCounter)){
                 i--;
+                continue;
             }
             
-            else if((i --) == 0 + rowCounter && j==(0 +rowCounter)){
+            else if((i-1) == 0 + rowCounter && j==(0 +rowCounter)){
+              
                 rowCounter ++;
+                j = j + 1;
+                continue;
             }
+            
+            
             
         }
-        
+       
         return ans;
+        
     }
 }
