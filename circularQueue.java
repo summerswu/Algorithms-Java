73d95af400152edf44d5343a5105b47471120e90
summers wu <wlysummers@gmail.com>
2019-04-25 03:09:06 -0400

    working version

--- circularQueue.java
+++ circularQueue.java
@@ -1,78 +1,131 @@
-//of course the previous method is shit because it waste space
-
 class MyCircularQueue {
 
     private int[] data;
 	private int tail;
 	private int head;
+    private int length;
     
     /** Initialize your data structure here. Set the size of the queue to be k. */
     public MyCircularQueue(int k) {
         data = new int[k];
-        head = 0;
-        tail = 0;
+        head = -1;
+        tail = -1;
+        length = 0;
     }
     
     /** Insert an element into the circular queue. Return true if the operation is successful. */
     public boolean enQueue(int value) {
        if(this.isFull())return false;
         
-        if((tail + 1) >= data.length){
-            tail = 0;
-        }
-        else tail++;
+        tail = (tail+1) % data.length;//
         data[tail] = value;
+        length++;
         return true;
     }
     
     /** Delete an element from the circular queue. Return true if the operation is successful. */
     public boolean deQueue() {
-        if(this.isEmpty()){
-            return false;
-        }
-            
+        if(this.isEmpty()) return false;
+
+        head = (head+1) % data.length;
         data[head] = 0;
-        
-        if(head + 1 >= data.length){
-            head = 0;
-        }
-        
-        else head++;
-        
-        if(tail==head && this.isEmpty()){
-             head = 0;
-             tail = 0;
-        }
-        
+        length--;
         return true;
         
     }
     
     /** Get the front item from the queue. */
     public int Front() {
-        return data[0];
+        if(this.isEmpty()) return -1; 
+        return data[head+1];
     }
     
     /** Get the last item from the queue. */
     public int Rear() {
-        return data[data.length];
+        if(this.isEmpty())return -1;            
+        return data[tail];
     }
     
     /** Checks whether the circular queue is empty or not. */
     public boolean isEmpty() {
-        if(tail==head)return true;
+        if(length==0)return true;
         return false;
     }
     
     /** Checks whether the circular queue is full or not. */
     public boolean isFull() {
-        for(int i = 0; i<data.length; i++){
-            if (data[i] != 0){
-                return false;
-            }
-        }
+       if(length==data.length)return true;
+        return false;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */class MyCircularQueue {
+
+    private int[] data;
+	private int tail;
+	private int head;
+    private int length;
+    
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue(int k) {
+        data = new int[k];
+        head = -1;
+        tail = -1;
+        length = 0;
+    }
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+       if(this.isFull())return false;
         
+        tail = (tail+1) % data.length;//
+        data[tail] = value;
+        length++;
         return true;
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if(this.isEmpty()) return false;
+
+        head = (head+1) % data.length;
+        data[head] = 0;
+        length--;
+        return true;
+        
+    }
+    
+    /** Get the front item from the queue. */
+    public int Front() {
+        if(this.isEmpty()) return -1; 
+        return data[head+1];
+    }
+    
+    /** Get the last item from the queue. */
+    public int Rear() {
+        if(this.isEmpty())return -1;            
+        return data[tail];
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        if(length==0)return true;
+        return false;
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+       if(length==data.length)return true;
+        return false;
     }
 }
 
