172fc1ca51eeaf62c66cc3433eb332113e160b5d
summers wu <wlysummers@gmail.com>
2019-05-01 21:27:25 -0400

    the final clean working stack version

--- parenthesis.java
+++ parenthesis.java
@@ -1,45 +1,18 @@
-class Solution {
+class Solution{
     public boolean isValid(String s) {
-        if(s.length()%2 ==1)return false;
-        ArrayList<Character> chars = new ArrayList<Character>();
-        for (char c : s.toCharArray()) {
-            chars.add(c);
-            System.out.print(c);
-        }
-        return checkLast(chars);
-        
+    Stack<Character> stack = new Stack<Character>();
+    for (char c : s.toCharArray()) {
+        if (c == '(')
+            stack.push(')');
+        else if (c == '{')
+            stack.push('}');
+        else if (c == '[')
+            stack.push(']');
+        else if (stack.isEmpty() || stack.pop() != c)
+            return false;
+        System.out.println("The C is " + c);
     }
+    return stack.isEmpty();
+}
     
-    public boolean checkLast(ArrayList chars){
-        if(chars.isEmpty())return true;
-        if(chars.size()==1)return false;
-        
-        char first = (char)chars.get(chars.size()-1);
-        chars.remove(chars.size()-1);
-       
-        if (first == ')'){
-            for(int i = chars.size()-2; i<=0; i--){
-                if((char)chars.get(i)=='(') {chars.remove(i);break;}
-                else if(i==0)return false;
-            }
-        }
-        
-        else if (first == '}'){
-            for(int i = chars.size()-2; i<=0; i--){
-                if((char)chars.get(i)=='{') {chars.remove(i); break;}
-                else if(i==0)return false;
-            }
-        }
-        
-        else if (first == ']'){
-            for(int i = chars.size()-2; i<=0; i--){
-                if((char)chars.get(i)=='[') {chars.remove(i); break;}
-                else if(i==0)return false;
-            }
-        }
-            
-        else return false;
-        checkLast(chars);
-        return true;
-    }
 }
