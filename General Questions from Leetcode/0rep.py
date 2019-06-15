14ecc68f772f1d1b9e22ea6b06198ab5e8797276
summers wu <wlysummers@gmail.com>
2019-06-14 22:29:30 -0400

    daily mock notes

---
+++ General Questions from Leetcode/0rep.py
@@ -0,0 +1,40 @@
+given m*m matrix
+
+if element is 0, set entire column and row to be zero
+
+Input: 
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+Output: 
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+
+
+
+0 is at arr[1][1]
+
+m * n
+
+arr[1][all]
+arr[all][1]
+
+loop throught the entire 2D array
+
+arraylist[x]
+arraylist[y]
+
+for i in range(0,m)
+	for x in range(0,n)
+  	 
+for i in range (0, n):
+	arr[x][n] == 0
+  
+for i in range (0, m):
+  arr[m][y] == 0
+  
