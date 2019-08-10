c49f7d745ec92fd928053d1be7cfbf146c04a22e
summers wu <wlysummers@gmail.com>
2019-08-10 14:50:27 -0400

    Moving data stream queue question solved

---
+++ Problems Categorized Using Data Structures/queue/movingaverage.java
@@ -0,0 +1,30 @@
+class MovingAverage {
+    
+    Queue<Integer> q;
+    int capacity;
+    int currSum;
+
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        q = new LinkedList<Integer>();
+        capacity = size;
+        currSum = 0;
+    }
+    
+    public double next(int val) {
+        currSum += val;
+        q.offer(val);
+        
+        if(q.size() > capacity ){
+            currSum -= q.poll();
+        }
+       
+        return currSum*1.0 / q.size() ;
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
