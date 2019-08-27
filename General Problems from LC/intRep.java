8ee1b3ec40725dbea6dfffffb8dc7debbc7b44cc
summers wu <wlysummers@gmail.com>
2019-08-27 19:01:13 -0400

    solved integer replacement using recursion, need to touch up on recursion time/space analysis

--- General Problems from LC/intRep.java
+++ General Problems from LC/intRep.java
@@ -1,12 +1,18 @@
 class Solution {
     public int integerReplacement(int n) {
-        int counter = 0;
-        if(n == 0)return counter;
-        if(n%2 != 0){n++;counter++;}
-        while (n != 1){
-            n = n/2;
-            counter++;
+        return integerRep((long)n);
+    }
+    
+    public int integerRep(long n){
+        int ans = 0;
+        
+        if(n <= 1) ans = 0;
+        
+        else if(n%2 == 0){
+            ans = 1 + integerRep(n/2);
+        } else {
+            ans = Math.min(1 + integerRep(n-1), 1 + integerRep(n+1));
         }
-        return counter;
+        return ans;
     }
 }
