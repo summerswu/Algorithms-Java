da53aed1d7a2154903649689c95afca818fbb5a7
summers wu <wlysummers@gmail.com>
2019-09-08 17:35:17 -0400

    solved k smallest element in BST problem

---
+++ Problems Categorized Using Data Structures/Graph/kSmallest.java
@@ -0,0 +1,13 @@
+ public int kthSmallest(TreeNode root, int k) {
+     Stack<TreeNode> stack = new Stack<>();
+     while(root != null || !stack.isEmpty()) {
+         while(root != null) {
+             stack.push(root);    
+             root = root.left;   
+         } 
+         root = stack.pop();
+         if(--k == 0) break;
+         root = root.right;
+     }
+     return root.val;
+}
