623d17eadb198b5645f8264af659d83c25ba055b
summers wu <wlysummers@gmail.com>
2019-09-13 15:41:39 -0400

    tidied up the code and came up with the working solution without additional arrayList

--- Real Interview Problem/Twitter/maximumValueWeight.java
+++ Real Interview Problem/Twitter/maximumValueWeight.java
@@ -26,28 +26,20 @@ class Result {
 
     public static int maximumTotalWeight(List<Integer> weights, List<Integer> tasks, int p) {
     // Write your code here
-        int max = 0;
-
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
-
-        for(int i = 0; i<weights.size(); i++){
-            ArrayList<Integer> task = new ArrayList<Integer>(Arrays.asList(tasks.get(i)*2, weights.get(i)));
-            arr.add(task);
-        }
 
         int[][] memo = new int[arr.size()][p+1];
 
         for(int i = 0; i< memo.length;i++){
             for(int j = 0; j <memo[i].length; j++){
                 if(i==0){
-                    if(arr.get(i).get(0)>j) memo[i][j] = 0;
-                    else memo[i][j] = arr.get(i).get(1);
+                    if(tasks.get(i)*2 > j) memo[i][j] = 0;
+                    else memo[i][j] = weights.get(i);
                 }
-                else if(arr.get(i).get(0)>j){
+                else if(tasks.get(i)>j){
                     memo[i][j] = memo[i-1][j];
                 }
                 else {
-                    int process = arr.get(i).get(1) + memo[i-1][j-arr.get(i).get(0)];
+                    int process = weights.get(i) + memo[i-1][j-tasks.get(i)*2];
                     int dontprocess = memo[i-1][j];
                     memo[i][j] = Math.max(process, dontprocess);
                 }
