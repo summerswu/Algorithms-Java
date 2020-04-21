c74cdcb13bae75c3401c2e782aa793b9990150b4
Summers Wu <wlysummers@gmail.com>
2020-04-21 14:16:25 -0400

    optimized using array instead of hashmap, however, still O(n^2)

--- Problmes Categorized Using Algorithms/Dynamic Programming/increasingSequence.java
+++ Problmes Categorized Using Algorithms/Dynamic Programming/increasingSequence.java
@@ -19,7 +19,29 @@ class Solution {
             dp.put(nums[i], maxCurr);
             max = Math.max(maxCurr, max);
         }
+            
+        return max;
+    }
+}
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        //longest increasing subsequence, not longest continuos subsequence
+        if(nums.length==0) return 0;
+        int max = 0;
         
+        int[] dp = new int[nums.length];
+        
+        for(int i = 0; i<nums.length; i++){
+            
+            dp[i] = 1;
+                
+            for(int j = 0; j<i; j++){
+                if(nums[j]<nums[i])
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+            }
+            max = Math.max(dp[i], max);
+        }
         return max;
     }
 }
